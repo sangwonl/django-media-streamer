49ea510707bd950ce147a445a919aae2eb305b4a
Sangwon Lee <gamzabaw@gmail.com>
2015-07-06 12:48:20 +0900

    removed force refresh of http response

--- streamer/views.py
+++ streamer/views.py
@@ -6,7 +6,7 @@ import re
 
 
 def get_stream(req, stream_name):
-    req.META.pop('HTTP_IF_MODIFIED_SINCE', None)
+    # stream_name could be '<media_token>.mpd' or '<media_token>_<bitrate>_<idx>.m4s'
     stream_token = re.split(r'[\._]+', stream_name)[0]
     media_root = os.path.join(base.MEDIA_ROOT, stream_token)
     return serve(request=req, path=stream_name, document_root=media_root, show_indexes=True)
